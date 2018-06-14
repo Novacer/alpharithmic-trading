6ee0d6361804498a697aaad8bda9335238addd5e
Jackamac-attack <zhangjack27@gmail.com>
2018-06-13 23:20:41 -0400

    installed django in venv

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\mary\documents\github\alpharithmic-trading\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
