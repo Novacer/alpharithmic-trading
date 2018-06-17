1d17eda2478c50f1c35a435b2ce505046d66f284
Jackamac-attack <zhangjack27@gmail.com>
2018-06-17 16:04:36 -0400

    buy_apple run now dynamically builds context & handle_data

--- backend/backend/algorithms/buy_apple.py
+++ backend/backend/algorithms/buy_apple.py
@@ -1,37 +1,24 @@
 from zipline.api import order, record, symbol
 from zipline import run_algorithm
 import pandas as pd
-import matplotlib.pyplot as plt
-import mpld3 as mp
 
 
-def initialize(context):
-    pass
+def run(shares_per_day, capital_base, start_date, end_date):
 
+    def init(context):
+        pass
 
-def handle_data(context, data):
-    order(symbol('AAPL'), 10)
-    record(AAPL=data.current(symbol('AAPL'), 'price'))
+    def handle(context, data):
+        order(symbol('AAPL'), shares_per_day)
+        record(AAPL=data.current(symbol('AAPL'), 'price'))
 
+    start = pd.to_datetime(start_date).tz_localize('US/Eastern')
+    end = pd.to_datetime(end_date).tz_localize('US/Eastern')
+    result = run_algorithm(start, end, capital_base=capital_base,
+                           initialize=init,
+                           handle_data=handle,
+                           bundle="quandl")
 
-def run():
-    start = pd.to_datetime('2015-01-01').tz_localize('US/Eastern')
-    end = pd.to_datetime('2016-01-01').tz_localize('US/Eastern')
-    result = run_algorithm(start, end, capital_base=100000,
-                           initialize=initialize,
-                           handle_data=handle_data, bundle="quandl")
+    result.dropna(inplace=True)
 
     return result
-
-
-data = run()
-data.dropna(inplace=True)
-
-print(data.head())
-
-plt.plot(data['benchmark_period_return'])
-
-fig = plt.gcf()
-
-print(mp.fig_to_html(fig))
-
