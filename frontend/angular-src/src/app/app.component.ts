ed7743c8ac436c776fdf3fa29084f54fbe0f7f94
Jackamac-attack <zhangjack27@gmail.com>
2018-06-24 15:41:49 -0400

    frontend can now call backend api, with proxy config

--- frontend/angular-src/src/app/app.component.ts
+++ frontend/angular-src/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+
+  private url : string;
+
+  constructor(private http: HttpClient) {
+    this.url = "/api/post/buy-apple"
+  }
+
+  ngOnInit() {
+    this.doApiCall();
+  }
+
+  doApiCall() {
+
+    let body = {
+      start: "2015-01-01",
+      end: "2016-01-01",
+      shares: 50,
+      capital_base: 1000000
+    };
+
+    this.http.post(this.url, body).subscribe(response => {
+      console.log(response);
+    });
+  }
 }
