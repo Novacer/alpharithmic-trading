d9f7cd747669374509ec236818e3f7aa4df69b49
Jackamac-attack <zhangjack27@gmail.com>
2018-07-08 16:50:24 -0400

    added bootstrap and navbar

--- frontend/angular-src/src/app/app.module.ts
+++ frontend/angular-src/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import { GraphComponent } from './graph/graph.component';
 import { ChartsModule } from "ng2-charts";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    GraphComponent
+    GraphComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    ChartsModule
+    ChartsModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
