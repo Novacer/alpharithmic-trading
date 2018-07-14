c807db8c850c92de4572bf0a166505fabdfd4620
Jackamac-attack <zhangjack27@gmail.com>
2018-07-14 12:23:56 -0400

    added rfr to select algo page

--- frontend/angular-src/src/app/algorithms/rand-forest-reg/rand-forest-reg.component.ts
+++ frontend/angular-src/src/app/algorithms/rand-forest-reg/rand-forest-reg.component.ts
@@ -25,6 +25,11 @@ export class RandForestRegComponent implements OnInit {
   constructor(private formBuilder: FormBuilder, private scroll: ScrollToService) { }
 
   ngOnInit() {
+
+    this.scroll.scrollTo({
+      target: 'top'
+    });
+
     this.firstForm =  this.formBuilder.group({firstCtrl: ['', Validators.required]});
     this.secondForm = this.formBuilder.group({secondCtrl: ['', Validators.required]});
     this.thirdForm = this.formBuilder.group({thirdCtrl: ['', Validators.required]});
