40942bb3dbc261fe62082e7f60738e455ccda1cd
Jackamac-attack <zhangjack27@gmail.com>
2018-07-15 16:40:22 -0400

    added routing to navbar

---
+++ frontend/angular-src/src/app/builder/builder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-builder',
+  templateUrl: './builder.component.html',
+  styleUrls: ['./builder.component.css']
+})
+export class BuilderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
