17139bd4bc5ce46877ef1f9a449c3f1a5459d117
Jackamac-attack <zhangjack27@gmail.com>
2018-08-27 11:28:22 -0700

    added websocket logging to trend follow

--- backend/backend/algorithms/trend_follow.py
+++ backend/backend/algorithms/trend_follow.py
@@ -7,12 +7,21 @@ from zipline.utils.events import date_rules, time_rules
 from zipline.pipeline.factors import AverageDollarVolume
 from zipline import run_algorithm
 
-import matplotlib.pyplot as plt
+from websocket import create_connection
+from ..api.create_response import create_json_response
 
 
-def trend_follow_run():
+def trend_follow_run(start_date, end_date, capital_base, log_channel):
+
+    ws = create_connection("ws://alpharithmic.herokuapp.com/ws/logs/%s/" % log_channel)
+    msg_placeholder = "{\"message\": \"%s\"}"
+
+    ws.send(msg_placeholder % "Link Start")
 
     def initialize(context):
+
+        ws.send(msg_placeholder % "Simulation Start")
+
         context.lookback = 252        # Period to calculate slope and drawdown
         context.max_leverage = 1.0    # Leverage
         context.profit_take = 1.96    # 95% of bollinger band
@@ -27,18 +36,27 @@ def trend_follow_run():
         schedule_function(func=stop_loss, date_rule=date_rules.every_day(),
                           time_rule=time_rules.market_open(minutes=30))
 
+        ws.send(msg_placeholder % "Execution of stop loss scheduled at 30 minutes after market open")
+
         schedule_function(func=regression, date_rule=date_rules.every_day(),
                           time_rule=time_rules.market_open(minutes=50))
 
+        ws.send(msg_placeholder % "Execution of regression computation scheduled at 50 minutes after market open")
+
         schedule_function(func=trade, date_rule=date_rules.every_day(),
                           time_rule=time_rules.market_open(minutes=100))
+
+        ws.send(msg_placeholder % "Execution of transaction planner scheduled at 100 minutes after market open")
 
         for thirty_minute_interval in range(30, 391, 30):
             schedule_function(execute_transactions, date_rules.every_day(),
                               time_rules.market_open(minutes=thirty_minute_interval))  # execute every 30 minutes
 
+        ws.send(msg_placeholder % "Execution of transactions scheduled at every 30 minutes")
+
         attach_pipeline(create_high_dollar_volume_pipeline(), 'top_dollar_volume')
 
+        ws.send(msg_placeholder % "High Dollar Volume pipeline filter attached")
 
     def create_high_dollar_volume_pipeline():
         pipe = Pipeline()
@@ -96,19 +114,32 @@ def trend_follow_run():
             gain = get_gain(context, s)
 
             # Exit
-            if s in context.weights and context.weights[s] != 0 :
+            if s in context.weights and context.weights[s] != 0:
 
                 # Long but slope turns down
                 if context.weights[s] > 0 and slope < 0:
                     context.weights[s] = 0
+                    ws.send(msg_placeholder % ('Gained %+2d%% for %s, exited from long because slope turns bull'
+                                               % (gain*100, str(s))))
 
                 # Short but slope turns up
                 elif context.weights[s] < 0 and slope > 0:
                     context.weights[s] = 0
+                    ws.send(msg_placeholder % ('Gained %+2d%% for %s, exited from short because slope turns bear'
+                                               % (gain*100, str(s))))
 
                 # Profit take reaches top 95% bollinger band
                 elif delta[-1] > context.profit_take * sd and s in context.weights and context.weights[s] > 0:
                     context.weights[s] = 0
+                    ws.send(msg_placeholder %
+                            ('Gained %+2d%% for %s, exited from long because profit take at top 95%% of bollinger band'
+                             % (gain * 100, str(s))))
+
+                elif delta[-1] < -context.profittake * sd and context.weights[s] < 0:
+                    context.weights[s] = 0
+                    ws.send(msg_placeholder %
+                            ('Gained %+2d%% for %s, exited from long because profit take at top 95%% of bollinger band'
+                             % (gain * 100, str(s))))
 
             # Enter
             else:
@@ -130,7 +161,15 @@ def trend_follow_run():
             if not data.can_trade(s) or s in open_orders:
                 continue
 
-            order_target_percent(s, context.shares[s])
+            num_shares = context.shares[s]
+
+            order_target_percent(s, num_shares)
+
+            if num_shares > 0:
+                ws.send("Bought %s shares of %s" % (str(num_shares), str(s)))
+
+            elif num_shares < 0:
+                ws.send("Shorted %s shares of %s" % (str(num_shares), str(s)))
 
     def trade(context, data):
         weights = context.weights
@@ -162,7 +201,7 @@ def trend_follow_run():
 
         for s in context.portfolio.positions:
 
-            if s not in context.weights or context.weights[s] == 0 :
+            if s not in context.weights or context.weights[s] == 0:
                 context.shares[s] = 0
                 continue
 
@@ -175,9 +214,17 @@ def trend_follow_run():
                 context.weights[s] = 0
                 context.shares[s] = 0  # stop loss
 
+                ws.send(msg_placeholder %
+                        ('Lost %+2d%% for %s, exited from long because of stop loss'
+                         % (gain * 100, str(s))))
+
             elif context.portfolio.positions[s].amount < 0 and drawdown(- prices[s].values) > context.drawdown[s]:
                 context.weights[s] = 0
                 context.shares[s] = 0
+
+                ws.send(msg_placeholder %
+                        ('Lost %+2d%% for %s, exited from short because of stop loss'
+                         % (gain * 100, str(s))))
 
     def drawdown(xs):
         if len(xs) == 0:
@@ -211,21 +258,18 @@ def trend_follow_run():
 
         return gain
 
-    start = pd.to_datetime("2015-01-01").tz_localize('US/Eastern')
-    end = pd.to_datetime("2016-01-01").tz_localize('US/Eastern')
+    start = pd.to_datetime(start_date).tz_localize('US/Eastern')
+    end = pd.to_datetime(end_date).tz_localize('US/Eastern')
 
     result = run_algorithm(start, end,
                            initialize=initialize, before_trading_start=before_trading_start,
-                           capital_base=1000000,
+                           capital_base=capital_base,
                            bundle="quantopian-quandl")
 
-    return result
+    ws.send(msg_placeholder % "Simulation End")
+    ws.send(msg_placeholder % "Fetching backtest results from Redis Queue...")
 
-res = trend_follow_run()
+    result.dropna(inplace=True)
+    ws.close()
 
-plt.plot(res['algorithm_period_return'], label='algo')
-plt.plot(res['benchmark_period_return'], label='bench')
-
-plt.legend()
-
-plt.show()
+    return create_json_response(result)
