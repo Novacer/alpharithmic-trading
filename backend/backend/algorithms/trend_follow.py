4f4072bed99a5a67627922aa5faf5f950f4838c4
Jackamac-attack <zhangjack27@gmail.com>
2018-08-27 09:22:38 -0700

    import schedule function

--- backend/backend/algorithms/trend_follow.py
+++ backend/backend/algorithms/trend_follow.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
-from zipline.api import attach_pipeline, pipeline_output
+from zipline.api import attach_pipeline, pipeline_output, schedule_function
 from zipline.pipeline import Pipeline
 from zipline.pipeline.data import USEquityPricing
 from zipline.pipeline.factors import AverageDollarVolume
