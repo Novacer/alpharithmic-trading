e0c5486e8560890353c3a2eac9a90634f457aafd
Jackamac-attack <zhangjack27@gmail.com>
2018-08-29 11:51:18 -0700

    implemented before trading start function

--- backend/backend/algorithms/regimes_clustering.py
+++ backend/backend/algorithms/regimes_clustering.py
@@ -11,9 +11,6 @@ from scipy import optimize
 from scipy import stats
 import statsmodels.api as sm
 
-# Date
-import datetime
-
 # Machine Learning
 from sklearn.cluster import KMeans
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
@@ -28,7 +25,7 @@ from collections import deque
 def regimes_clustering_run():
 
     def initialize(context):
-        context.security = symbol("AAPL")
+        context.security_list = []
         context.long_threshold = 0
         context.short_threshold = -0.06
 
@@ -39,7 +36,7 @@ def regimes_clustering_run():
 
         context.n_clusters = 9
         context.ret_windows = [30]
-        context.window_length = [15, 30, 60, 90, 120, 150, 180, 210, 240]
+        context.window_lengths = [15, 30, 60, 90, 120, 150, 180, 210, 240]
         context.lookback = 8 * 250
         context.refresh_frequency = 30
 
@@ -58,15 +55,83 @@ def regimes_clustering_run():
         context.price_projections = {}
         context.bucket_probs = {}
 
-        attach_pipeline(create_high_dollar_volume_pipeline(), 'top_dollar_volume')
+        context.days_traded = 0
+
+        attach_pipeline(create_high_dollar_volume_pipeline(), 'tdv')
 
     def create_high_dollar_volume_pipeline():
         pipe = Pipeline()
 
-        dollar_volume = AverageDollarVolume(window_length=63)  # 63 days = 1 quarter
+        dollar_volume = AverageDollarVolume(window_length=63)
         pipe.add(dollar_volume, 'dollar_volume')
 
-        high_dollar_volume = dollar_volume.percentile_between(95, 100)  # top 5% by dollar volume
+        high_dollar_volume = dollar_volume.percentile_between(99, 100)  # top 1% by dollar volume
         pipe.set_screen(high_dollar_volume)
 
         return pipe
+
+    def before_trading_start(context, data):
+        context.output = pipeline_output('tdv')
+        context.security_list = context.output.index
+
+        context.days_traded += 1
+
+        if context.model == {} or context.model['refresh_date'] <= context.days_traded:
+            context.model['refresh_date'] = context.days_traded + context.refresh_frequency
+            clusters = {}
+
+            for ret_window in context.ret_windows:
+                clusters[ret_window] = {'windows': {}}
+
+                for window_length in context.window_lengths:
+                    cluster_data = get_cluster_data(context, data, window_length, ret_window)
+                    cluster_data.dropna(inplace=True)
+                    X = cluster_data.drop('rets', axis=1)
+                    y = cluster_data['rets']
+
+                    kmeans = KMeans(n_clusters=context.n_clusters, n_init=100, max_iter=500, random_state=42)
+                    kmeans.fit(X)
+                    clusters[ret_window]['windows'][window_length] = {
+                        "kmeans": kmeans,
+                        "regimes": kmeans.predict(X),
+                        "rets": y
+                    }
+
+            panel = get_X(clusters)
+
+            for ret_window, _ in clusters.items():
+                df = panel[ret_window]
+                ret = df['rets']
+
+                X = df.drop('rets', axis=1)
+                X_train = X.values
+
+                if context.use_classifier:
+
+                    global ret_buckets
+
+                    try:
+                        ret_buckets = context.ret_buckets[ret_window]
+                    except KeyError:
+                        ret_buckets = context.ret_buckets['gen']
+
+                    clf = OneVsRestClassifier(RandomForestClassifier(n_estimators=1000, random_state=42))
+
+                    y = len(ret_buckets) * np.ones(len(ret)).astype(int)
+
+                    for i in range(len(ret_buckets) - 1, -1, -1):
+                        I = ret.values < ret_buckets[i]
+
+                        y[I] = i
+
+                    y_train = y
+
+                    clf.fit(X_train, y_train)
+                    clusters[ret_window]['clf'] = clf
+
+                else:
+                    rfr = RandomForestRegressor(n_estimators=1000, random_state=42)
+                    rfr.fit(X_train, ret.values)
+                    clusters[ret_window]['reg'] = rfr
+
+            context.model['clusters'] = clusters
