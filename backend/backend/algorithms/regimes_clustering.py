6ba898c210e47c8c6766eff7af58c8c0bceaba49
Jackamac-attack <zhangjack27@gmail.com>
2018-08-29 11:09:37 -0700

    started initialize function

--- backend/backend/algorithms/regimes_clustering.py
+++ backend/backend/algorithms/regimes_clustering.py
@@ -1,5 +1,5 @@
 # Zipline API
-from zipline.api import attach_pipeline, pipeline_output
+from zipline.api import attach_pipeline, pipeline_output, symbol, set_long_only
 from zipline.pipeline import Pipeline
 from zipline.pipeline.data import USEquityPricing
 from zipline.pipeline.factors import AverageDollarVolume
@@ -11,6 +11,9 @@ from scipy import optimize
 from scipy import stats
 import statsmodels.api as sm
 
+# Date
+import datetime
+
 # Machine Learning
 from sklearn.cluster import KMeans
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
@@ -20,3 +23,37 @@ from sklearn.metrics import classification_report, confusion_matrix
 
 # Data structures
 from collections import deque
+
+
+def regimes_clustering_run():
+
+    def initialize(context):
+        context.security = symbol("AAPL")
+        context.long_threshold = 0
+        context.short_threshold = -0.06
+
+        context.no_shorts = False
+
+        if context.no_shorts:
+            set_long_only()
+
+        context.n_clusters = 9
+        context.ret_windows = [30]
+        context.window_length = [15, 30, 60, 90, 120, 150, 180, 210, 240]
+        context.lookback = 8 * 250
+        context.refresh_frequency = 30
+
+        context.use_classifier = True
+
+        if context.use_classifier:
+            context.ret_buckets = {
+                "gen": [-0.04, 0, 0.04]
+            }
+
+        context.long_prob_lb = 0.5
+        context.short_prob_ub = 0.3
+
+        context.model = {}
+        context.return_projections = {}
+        context.price_projections = {}
+        context.bucket_probs = {}
