655e8400d7b9c40f4061f703dba4be325e72873a
Jackamac-attack <zhangjack27@gmail.com>
2019-08-13 19:48:46 -0700

    Add editor service to call default src code endpoint

---
+++ frontend/angular-src/src/app/services/editor.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs/internal/Observable";
+import {map} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EditorService {
+
+  private readonly defaultSrcURL = '/api/get/default-src-code';
+
+  constructor(private http: HttpClient) { }
+
+  getDefaultSrcCode(): Observable<string> {
+    return this.http.get(this.defaultSrcURL).pipe(
+      map((response: DefaultSrcResponse) => response.code)
+    );
+  }
+}
+
+interface DefaultSrcResponse {
+  code: string;
+}
