0270b63398dc98644d5603fd9f0155059f3bcbdf
Jackamac-attack <zhangjack27@gmail.com>
2019-08-14 17:59:39 -0700

    Implemented basic code execution endpoint

--- backend/backend/api/source_code_executor.py
+++ backend/backend/api/source_code_executor.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 import os
 
+from ..algorithms.custom_algos.template_algorithm import template_algorithm
+
 
 class GetDefaultSourceCodeFile(APIView):
     def get(self, request, format=None):
@@ -10,3 +12,14 @@ class GetDefaultSourceCodeFile(APIView):
             code = file.read()
             json = {'code': code}
             return Response(json)
+
+
+class ExecuteSourceCode(APIView):
+    def post(self, request, format=None):
+        json = template_algorithm(request.data['src_code'],
+                                  request.data['capital_base'],
+                                  request.data['start'],
+                                  request.data['end'],
+                                  request.data['log_channel'])
+        return Response(json)
+
