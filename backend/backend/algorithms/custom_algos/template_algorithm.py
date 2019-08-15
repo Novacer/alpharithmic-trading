2af03a2f3396aa4944465e29b7cd72edc1f526d7
Jackamac-attack <zhangjack27@gmail.com>
2019-08-15 00:59:37 -0700

    code executor now logs compile/runtime errors to client

--- backend/backend/algorithms/custom_algos/template_algorithm.py
+++ backend/backend/algorithms/custom_algos/template_algorithm.py
@@ -13,6 +13,7 @@ from .allowed_imports import GLOBAL_FIELDS
 from backend.api.logger import Logger
 
 from backend.api.create_response import create_json_response
+from .filter_error_msg import filter_error_msg
 
 
 def template_algorithm(src_code, capital_base, start_date, end_date, log_channel):
@@ -25,19 +26,40 @@ def template_algorithm(src_code, capital_base, start_date, end_date, log_channel
     local_state = {}
 
     byte_code = compile_restricted_exec(src_code)
+
+    if len(byte_code.errors) != 0:
+        log('COMPILE ERROR =====')
+        for error in byte_code.errors:
+            log(error)
+        return {
+            'done': True,
+            'success': False,
+        }
+
     exec(byte_code.code, {**GLOBAL_FIELDS, **local_fields}, local_state)  # local state may be modified
 
     start = pd.to_datetime(start_date).tz_localize('US/Eastern')
     end = pd.to_datetime(end_date).tz_localize('US/Eastern')
 
-    result = run_algorithm(start, end,
-                           initialize=local_state.get('initialize'),
-                           handle_data=local_state.get('handle_data'),
-                           before_trading_start=local_state.get('before_trading_start'),
-                           capital_base=capital_base,
-                           bundle='quandl')
+    try:
+        result = run_algorithm(start, end,
+                               initialize=local_state.get('initialize'),
+                               handle_data=local_state.get('handle_data'),
+                               before_trading_start=local_state.get('before_trading_start'),
+                               capital_base=capital_base,
+                               bundle='quandl')
 
-    result.dropna(inplace=True)
-    logger.close()
+        result.dropna(inplace=True)
+        logger.close()
+        return create_json_response(result)
+    except Exception as e:
+        error_msg = str(e)
+        log('RUNTIME ERROR ====')
 
-    return create_json_response(result)
+        if not filter_error_msg(error_msg):
+            log(error_msg)
+
+        return {
+            'done': True,
+            'success': False,
+        }
